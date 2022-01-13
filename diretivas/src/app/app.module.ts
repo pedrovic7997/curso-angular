026c962c15c68bf6d8094b62bf811531ac85c2cf
pedrovic7997 <pedrovictor6974@gmail.com>
2022-01-13 12:25:28 -0300

    Manipulando classes HTML usando diretiva ngClass

--- diretivas/src/app/app.module.ts
+++ diretivas/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
 import { DiretivaNgswitchComponent } from './diretiva-ngswitch/diretiva-ngswitch.component';
 import { DireticaNgforComponent } from './diretica-ngfor/diretica-ngfor.component';
+import { DiretivaNgclassComponent } from './diretiva-ngclass/diretiva-ngclass.component';
+import { DiretivaNgstyleComponent } from './diretiva-ngstyle/diretiva-ngstyle.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DiretivaNgifComponent,
     DiretivaNgswitchComponent,
-    DireticaNgforComponent
+    DireticaNgforComponent,
+    DiretivaNgclassComponent,
+    DiretivaNgstyleComponent
   ],
   imports: [
     BrowserModule,
