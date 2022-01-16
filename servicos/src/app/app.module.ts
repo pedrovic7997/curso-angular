39a912bb6ba8888e5b9be37c051316a3d1a88a1f
pedrovic7997 <pedrovictor6974@gmail.com>
2022-01-16 18:23:54 -0300

    Injetando um Servico dentro de outro Servico

--- servicos/src/app/app.module.ts
+++ servicos/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 
 import { CriarCursoModule } from './criar-curso/criar-curso.module';
 import { CursosModule } from './cursos/cursos.module';
+import { LogService } from './shared/log.service';
 
 @NgModule({
   declarations: [
@@ -17,6 +18,7 @@ import { CursosModule } from './cursos/cursos.module';
   ],
   providers: [
     // CursosService
+    LogService
   ],
   bootstrap: [AppComponent]
 })
