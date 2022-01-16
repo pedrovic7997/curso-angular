e91144f42ddc128a7e5d32c92d180a8510224d4e
pedrovic7997 <pedrovictor6974@gmail.com>
2022-01-15 23:44:07 -0300

    Injecao de servicos Singleton vs varias instancias

--- servicos/src/app/app.module.ts
+++ servicos/src/app/app.module.ts
@@ -1,22 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { CursosService } from './cursos/cursos.service';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CursosComponent } from './cursos/cursos.component';
+
+import { CursosService } from './cursos/cursos.service';
+import { CriarCursoModule } from './criar-curso/criar-curso.module';
+import { CursosModule } from './cursos/cursos.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CursosComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    CriarCursoModule,
+    CursosModule
   ],
   providers: [
-    CursosService
+    // CursosService
   ],
   bootstrap: [AppComponent]
 })
